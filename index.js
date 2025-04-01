9c5ef4630ecc1c749b7a74d0e9746eccda967bb9
dylandevops <163369822+dylandevops@users.noreply.github.com>
2025-04-01 11:10:13 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node.js on Azure Web App deployed via GitHub!');
+});
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});
