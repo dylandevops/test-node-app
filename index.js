ba0e15976b5cb8b26cb1b25d6c6c47b00b8c837d
dylandevops <163369822+dylandevops@users.noreply.github.com>
2025-05-20 14:38:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js on Azure Web App deployed via GitHub by UIH!');
+    res.send('Hello from Node.js on Azure Web App Staging Environment deployed via GitHub by UIH!');
 });
 
 app.listen(port, () => {
